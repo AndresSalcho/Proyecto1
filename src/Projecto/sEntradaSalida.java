69da649b263565efa410dd7a8e237a8f17d37a7b
elsrs <handresalve@gmail.com>
2023-11-27 17:59:30 -0600

    a

--- src/Projecto/sEntradaSalida.java
+++ src/Projecto/sEntradaSalida.java
@@ -34,9 +34,9 @@ public class sEntradaSalida {
             index++;
         }
 
-        IndividualSeparator(Local, "l");
-        IndividualSeparator(Retiro, "r");
-        IndividualSeparator(Entrega, "e");
+        Local = ul.IndividualSeparator(Local, "l");
+        Retiro = ul.IndividualSeparator(Retiro, "r");
+        Entrega = ul.IndividualSeparator(Entrega, "e");
 
         fechaL = u.getFechas(Local);
         fechaR = u.getFechas(Retiro);
@@ -110,140 +110,6 @@ public class sEntradaSalida {
         } catch (FileNotFoundException e) {
             System.out.println("Error al abrir el archivo!!!");
             return;
-        }
-    }
-
-    private void IndividualSeparator(String[] s, String id){
-        char[] c;
-        boolean ready = false;
-        for (int j = 1; j < s.length; j++) {
-            int pos = 0;
-            c = s[j].toCharArray();
-            for (int i = 0; i < c.length; i++) {
-                if (c[i] == ';' && pos == 0) {
-                    aux = i;
-                    if (id.equals("l")) {
-                        Local[j - 1] = "Fecha: ";
-                    }
-                    if (id.equals("r")) {
-                        Retiro[j - 1] = "Fecha: ";
-                    }
-                    if (id.equals("e")) {
-                        Entrega[j - 1] = "Fecha: ";
-                    }
-                    for (int k = 0; k < aux; k++){
-                        if (id.equals("l")) {
-                            Local[j - 1] += c[k];
-                        }
-                        if (id.equals("r")) {
-                            Retiro[j - 1] += c[k];
-                        }
-                        if (id.equals("e")) {
-                            Entrega[j - 1] += c[k];
-                        }
-                    }
-                    ready = true;
-                }else if (c[i] == ';' && pos == 1) {
-                    if (id.equals("l")) {
-                        Local[j - 1] += " | Venta: ";
-                    }
-                    if (id.equals("r")) {
-                        Retiro[j - 1] += " | Venta: ";
-                    }
-                    if (id.equals("e")) {
-                        Entrega[j - 1] += " | Venta: ";
-                    }
-                    for (int k = aux + 1; k < i; k++){
-                        if (id.equals("l")) {
-                            Local[j - 1] += c[k];
-                        }
-                        if (id.equals("r")) {
-                            Retiro[j - 1] += c[k];
-                        }
-                        if (id.equals("e")) {
-                            Entrega[j - 1] += c[k];
-                        }
-                    }
-                    aux = i;
-                    ready = true;
-                }else if (c[i] == ';' && pos == 2) {
-                    if (id.equals("l")) {
-                        Local[j - 1] += " | Costo: ";
-                    }
-                    if (id.equals("r")) {
-                        Retiro[j - 1] += " | Costo: ";
-                    }
-                    if (id.equals("e")) {
-                        Entrega[j - 1] += " | Costo: ";
-                    }
-                    for (int k = aux + 1; k < i; k++){
-                        if (id.equals("l")) {
-                            Local[j - 1] += c[k];
-                        }
-                        if (id.equals("r")) {
-                            Retiro[j - 1] += c[k];
-                        }
-                        if (id.equals("e")) {
-                            Entrega[j - 1] += c[k];
-                        }
-                    }
-                    aux = i;
-                    ready = true;
-                    if (!id.equals("e")) {
-                        i -= 2;
-                        save = c.length;
-                    }
-                }else if (c[i] == ';' && pos == 3) {
-                    if (id.equals("l")) {
-                        Local[j - 1] += " | Impuesto: ";
-                    }
-                    if (id.equals("r")) {
-                        Retiro[j - 1] += " | Empaque: ";
-                    }
-                    if (id.equals("e")) {
-                        Entrega[j - 1] += " | Envio: ";
-                    }
-                    if (id.equals("e")) {
-                        save = i;
-                    }
-                    for (int k = aux + 1; k < save; k++){
-                        if (id.equals("l")) {
-                            Local[j - 1] += c[k];
-                        }
-                        if (id.equals("r")) {
-                            Retiro[j - 1] += c[k];
-                        }
-                        if (id.equals("e")) {
-                            Entrega[j - 1] += c[k];
-                        }
-                    }
-                    aux = i;
-                    ready = true;
-                    if (id.equals("e")) {
-                        i -= 2;
-                    }
-                }else if (c[i] == ';' && pos == 4 && id.equals("e")) {
-                    Entrega[j - 1] += " | Entregado: ";
-                    for (int k = i + 1; k < c.length; k++) {
-                        Entrega[j - 1] += c[k];
-                    }
-                    aux = i;
-                    ready = true;
-                }else if (ready){
-                    ready = false;
-                    pos++;
-                }
-            }
-
-        }
-        if (id.equals("l")) {
-            Local[Local.length-1] = "--------------------------------------------------------------------";
-        }
-        if (id.equals("r")) {
-            Retiro[Local.length-1] = "--------------------------------------------------------------------";
-        }
-        if (id.equals("e")) {
-            Entrega[Local.length-1] = "--------------------------------------------------------------------";
         }
     }
 }
--- src/Projecto/uLogica.java
+++ src/Projecto/uLogica.java
@@ -76,5 +76,143 @@ public class uLogica {
     public String[] getTotal(String[] a){
         return a;
     }
+
+    public String[] IndividualSeparator(String[] s, String id){
+        char[] c;
+        boolean ready = false;
+        int aux = 0;
+        int save = 0;
+        for (int j = 1; j < s.length; j++) {
+            int pos = 0;
+            c = s[j].toCharArray();
+            for (int i = 0; i < c.length; i++) {
+                if (c[i] == ';' && pos == 0) {
+                    aux = i;
+                    if (id.equals("l")) {
+                        s[j - 1] = "Fecha: ";
+                    }
+                    if (id.equals("r")) {
+                        s[j - 1] = "Fecha: ";
+                    }
+                    if (id.equals("e")) {
+                        s[j - 1] = "Fecha: ";
+                    }
+                    for (int k = 0; k < aux; k++){
+                        if (id.equals("l")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("r")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("e")) {
+                            s[j - 1] += c[k];
+                        }
+                    }
+                    ready = true;
+                }else if (c[i] == ';' && pos == 1) {
+                    if (id.equals("l")) {
+                        s[j - 1] += " | Venta: ";
+                    }
+                    if (id.equals("r")) {
+                        s[j - 1] += " | Venta: ";
+                    }
+                    if (id.equals("e")) {
+                        s[j - 1] += " | Venta: ";
+                    }
+                    for (int k = aux + 1; k < i; k++){
+                        if (id.equals("l")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("r")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("e")) {
+                            s[j - 1] += c[k];
+                        }
+                    }
+                    aux = i;
+                    ready = true;
+                }else if (c[i] == ';' && pos == 2) {
+                    if (id.equals("l")) {
+                        s[j - 1] += " | Costo: ";
+                    }
+                    if (id.equals("r")) {
+                        s[j - 1] += " | Costo: ";
+                    }
+                    if (id.equals("e")) {
+                        s[j - 1] += " | Costo: ";
+                    }
+                    for (int k = aux + 1; k < i; k++){
+                        if (id.equals("l")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("r")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("e")) {
+                            s[j - 1] += c[k];
+                        }
+                    }
+                    aux = i;
+                    ready = true;
+                    if (!id.equals("e")) {
+                        i -= 2;
+                        save = c.length;
+                    }
+                }else if (c[i] == ';' && pos == 3) {
+                    if (id.equals("l")) {
+                        s[j - 1] += " | Impuesto: ";
+                    }
+                    if (id.equals("r")) {
+                        s[j - 1] += " | Empaque: ";
+                    }
+                    if (id.equals("e")) {
+                        s[j - 1] += " | Envio: ";
+                    }
+                    if (id.equals("e")) {
+                        save = i;
+                    }
+                    for (int k = aux + 1; k < save; k++){
+                        if (id.equals("l")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("r")) {
+                            s[j - 1] += c[k];
+                        }
+                        if (id.equals("e")) {
+                            s[j - 1] += c[k];
+                        }
+                    }
+                    aux = i;
+                    ready = true;
+                    if (id.equals("e")) {
+                        i -= 2;
+                    }
+                }else if (c[i] == ';' && pos == 4 && id.equals("e")) {
+                    s[j - 1] += " | Entregado: ";
+                    for (int k = i + 1; k < c.length; k++) {
+                        s[j - 1] += c[k];
+                    }
+                    aux = i;
+                    ready = true;
+                }else if (ready){
+                    ready = false;
+                    pos++;
+                }
+            }
+
+        }
+        if (id.equals("l")) {
+            s[s.length-1] = "--------------------------------------------------------------------";
+        }
+        if (id.equals("r")) {
+            s[s.length-1] = "--------------------------------------------------------------------";
+        }
+        if (id.equals("e")) {
+            s[s.length-1] = "--------------------------------------------------------------------";
+        }
+
+        return s;
+    }
 }
 
