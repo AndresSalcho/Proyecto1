f74b6eb0314c7fbd89e0e3b496758f770595d691
elsrs <handresalve@gmail.com>
2023-11-27 23:29:37 -0600

    a

---
+++ Abrir en CMD.bat
@@ -0,0 +1,3 @@
+javac -cp out\production\Proyecto1;src\Projecto\aspose-cells-23.11.jar src\Projecto\Main.java
+java -cp out\production\Proyecto1;src\Projecto\aspose-cells-23.11.jar Projecto.Main -l D:\Descargas\ventas_en_local.csv -r D:\Descargas\ventas_para_retirar.csv -e D:\Descargas\ventas_con_entrega.csv -m 8
+PAUSE
