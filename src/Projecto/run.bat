542c6d87afc0e1efdd45530eff522a1a72f92ad7
elsrs <handresalve@gmail.com>
2023-11-27 23:29:32 -0600

    a

--- src/Projecto/run.bat
+++
@@ -1,3 +0,0 @@
-javac -cp ..;aspose-cells-23.11.jar Main.java
-java -cp ..;aspose-cells-23.11.jar Main.java -l "D:\Descargas\Local.csv" -r "D:\Descargas\Retiro.csv" -e "D:\Descargas\Entrega.csv" -m 8
-PAUSE
